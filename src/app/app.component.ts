e909f1119f7c17aaf2cc72a2bd8458bc73fb46b5
Italo Correia <italocdaraujo@gmail.com>
2025-09-29 20:03:41 -0300

    feat: add metadata

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,7 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from './shared/header/header.component';
+import { Meta, Title } from '@angular/platform-browser';
 
 @Component({
   selector: 'app-root',
@@ -9,6 +10,27 @@ import { HeaderComponent } from './shared/header/header.component';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'sulwork-cafe-web';
+export class AppComponent implements OnInit {
+  constructor(private meta: Meta, private title: Title) {}
+
+  ngOnInit() {
+    const pageTitle = 'Sulwork Café | Organize seu café da manhã';
+    const pageDescription = 'Uma aplicação para organizar os cafés da manhã colaborativos da sua equipe.';
+    const imageUrl = 'https://sulwork-cafe-frontend-production.up.railway.app/assets/social-preview.jpg';
+    const pageUrl = 'https://sulwork-cafe-frontend-production.up.railway.app';
+
+    this.title.setTitle(pageTitle);
+    
+    this.meta.updateTag({ name: 'description', content: pageDescription });
+
+    this.meta.updateTag({ property: 'og:title', content: pageTitle });
+    this.meta.updateTag({ property: 'og:description', content: pageDescription });
+    this.meta.updateTag({ property: 'og:image', content: imageUrl });
+    this.meta.updateTag({ property: 'og:url', content: pageUrl });
+
+    this.meta.updateTag({ name: 'twitter:card', content: 'summary_large_image' });
+    this.meta.updateTag({ name: 'twitter:title', content: pageTitle });
+    this.meta.updateTag({ name: 'twitter:description', content: pageDescription });
+    this.meta.updateTag({ name: 'twitter:image', content: imageUrl });
+  }
 }
